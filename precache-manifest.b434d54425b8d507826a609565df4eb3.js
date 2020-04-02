e306c97a47601d890a4e6a66374ad8af16b4a61d
張晴惠 <sunny70624@gmail.com>
2020-04-02 10:51:54 +0800

    Updates

---
+++ precache-manifest.b434d54425b8d507826a609565df4eb3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048d34a8687f6603c560a18f85b0e6af",
+    "url": "/dcard_hui/index.html"
+  },
+  {
+    "revision": "74d1a1cc976c04abb3ae",
+    "url": "/dcard_hui/static/css/main.596f29ea.chunk.css"
+  },
+  {
+    "revision": "4b67578935d5e1602b76",
+    "url": "/dcard_hui/static/js/2.5b381b84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dcard_hui/static/js/2.5b381b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d1a1cc976c04abb3ae",
+    "url": "/dcard_hui/static/js/main.5c2a72c9.chunk.js"
+  },
+  {
+    "revision": "d989108db2a9262758fc",
+    "url": "/dcard_hui/static/js/runtime-main.5711ed89.js"
+  },
+  {
+    "revision": "4d3c14b97c7e45b33b87b6b8e1e2ea78",
+    "url": "/dcard_hui/static/media/img1.4d3c14b9.jpg"
+  },
+  {
+    "revision": "1e830096dff83be2cf2ba9384f7db49f",
+    "url": "/dcard_hui/static/media/img2.1e830096.jpg"
+  },
+  {
+    "revision": "2554223aefe6b57ed34f22d9d239c942",
+    "url": "/dcard_hui/static/media/img3.2554223a.jpg"
+  },
+  {
+    "revision": "8cb283ac7ea6f9960dc4e358425c17a3",
+    "url": "/dcard_hui/static/media/img4.8cb283ac.jpg"
+  }
+]);
